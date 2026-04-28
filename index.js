9871a757587a9c98b698c7c3dfa9a31463967ba9
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-04-28 20:06:06 +0300

    express-validator

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const {
+  query,
+  validationResult,
+  body,
+  matchedData,
+  checkSchema,
+} = require("express-validator");
+const createUserValidationSchema = require("./validationSchemas");
 
 const app = express();
 
@@ -42,24 +50,42 @@ app.get("/", loggingMiddleware, (req, res) => {
   res.status(201).send({ msg: "Hello" });
 });
 
-app.get("/api/users", (req, res) => {
-  console.log(req.query);
-  const {
-    query: { filter, value },
-  } = req;
+app.get(
+  "/api/users",
+  query("filter")
+    .isString()
+    .notEmpty()
+    .withMessage("Must not be empty")
+    .isLength({ min: 3, max: 10 })
+    .withMessage("Must be at least 3 - 10 characters"),
+  (req, res) => {
+    const result = validationResult(req);
+    console.log(result);
+    const {
+      query: { filter, value },
+    } = req;
 
-  // when filter and value are undefined
+    // when filter and value are undefined
 
-  if (filter && value) {
-    return res.send(mockUsers.filter((user) => user[filter].includes(value)));
-  }
-  return res.send(mockUsers);
-});
+    if (filter && value) {
+      return res.send(mockUsers.filter((user) => user[filter].includes(value)));
+    }
+    return res.send(mockUsers);
+  },
+);
 
-app.post("/api/users", (req, res) => {
+app.post("/api/users", checkSchema(createUserValidationSchema), (req, res) => {
   console.log(req.body);
-  const { body } = req;
-  const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body };
+  const result = validationResult(req);
+  console.log(result);
+
+  if (!result.isEmpty()) {
+    return res.status(400).send({ errors: result.array() });
+  }
+
+  const data = matchedData(req);
+
+  const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...data };
   mockUsers.push(newUser);
   return res.status(201).send(newUser);
 });
---
+++ validationSchemas.js
@@ -0,0 +1,22 @@
+const createUserValidationSchema = {
+  username: {
+    isLength: {
+      options: {
+        min: 5,
+        max: 32,
+      },
+      errorMessage: "Username must be atleast 5 - 32 characters",
+    },
+    notEmpty: {
+      errorMessage: "Username cannot be empty",
+    },
+    isString: {
+      errorMessage: "Username must be a string",
+    },
+  },
+  displayName: {
+    notEmpty: true,
+  },
+};
+
+module.exports = createUserValidationSchema;
