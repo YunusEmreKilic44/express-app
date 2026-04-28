6ab0b189c5efb8621421cf66b6498d9349404c40
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-04-28 15:16:02 +0300

    put request

--- index.js
+++ index.js
@@ -64,6 +64,23 @@ app.get("/api/products", (req, res) => {
   res.send([{ id: 123, name: "chicken breast", price: 12.99 }]);
 });
 
+app.put("/api/users/:id", (req, res) => {
+  const {
+    body,
+    params: { id },
+  } = req;
+
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) return res.sendStatus(400);
+
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+
+  if (findUserIndex === -1) return res.statusCode(404);
+
+  mockUsers[findUserIndex] = { id: parsedId, ...body };
+  return res.sendStatus(200);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}...`);
 });
