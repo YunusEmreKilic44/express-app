521f0682ddc052f029c11069b57859c44e3edd4f
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-01 16:03:28 +0300

    auth

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 const usersRouter = require("./routes/users");
 const productsRouter = require("./routes/products");
+const mockUsers = require("./mockUsers");
 
 app.use(express.json());
 app.use(cookieParser("helloworld"));
@@ -34,6 +35,48 @@ app.get("/", (req, res) => {
     signed: true,
   });
   res.status(201).send({ msg: "Hello" });
+});
+
+app.post("/api/auth", (req, res) => {
+  const {
+    body: { username, password },
+  } = req;
+
+  const findUser = mockUsers.find((user) => user.username === username);
+  if (!findUser || findUser.password !== password)
+    return res.status(401).send({ msg: "BAD CREDENTIALS" });
+
+  req.session.user = findUser;
+  return res.status(200).send(findUser);
+});
+
+app.get("/api/auth/status", (req, res) => {
+  req.sessionStore.get(req.sessionID, (err, session) => {
+    console.log(session);
+  });
+  return req.session.user
+    ? res.status(200).send(req.session.user)
+    : res.status(401).send({ msg: "Not Authenticated" });
+});
+
+app.post("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+
+  const { body: item } = req;
+
+  const { cart } = req.session;
+
+  if (cart) {
+    cart.push(item);
+  } else {
+    req.session.cart = [item];
+  }
+  return res.status(201).send(item);
+});
+
+app.get("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+  return res.send(req.session.cart ?? []);
 });
 
 app.listen(PORT, () => {
